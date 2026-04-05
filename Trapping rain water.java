3629f5ac5dcd839c634037c37b0bf2305ebaf57e
Sri Rangan <srirangan0603@gmail.com>
2026-04-05 21:59:33 +0530

    Create Trapping rain water.java

---
+++ Trapping rain water.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int trap(int[] height) {
+         
+
+         int n=height.length;
+         int left=0;
+         int right=n-1;
+
+         int leftmax=0;
+         int rightmax=0;
+         int ans=0;
+
+         while(left<right){
+            leftmax=Math.max(leftmax,height[left]);
+            rightmax=Math.max(rightmax,height[right]);
+
+
+            if(leftmax < rightmax){
+                ans+=leftmax-height[left];
+                left++;
+            }
+
+            else{
+                ans+=rightmax-height[right];
+                right--;
+            }
+
+         }
+
+
+         return ans;
+        
+    }
+}
